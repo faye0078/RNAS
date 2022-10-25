d0bc6eee310c91c7bf766397da82baf98c8d91aa
wy <faye892322@gmail.com>
2022-10-25 21:01:43 +0800

    add tools

---
+++ tools/pretrain.py
@@ -0,0 +1,12 @@
+# train the supernet
+import argparse
+from configs.pretrain_config import obtain_pretrain_args
+
+
+def main():
+    
+    args = obtain_pretrain_args()
+    
+if __name__ == '__main__':
+    main()
+    
